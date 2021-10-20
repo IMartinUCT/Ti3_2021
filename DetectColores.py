91977030698f6cb7a365d264b9ace0e963ba4d6d
LILEnrique <31453489+LILEnrique@users.noreply.github.com>
2021-10-19 23:28:24 -0300

    Detección de células (normales y colores) V2
    Se modificó el código base con el fin de reducir los tiempos de respuestas y además sacar procesos innecesarios. Apegándonos a los consejos del profesor Julio y el Sr. Marcos, restructuramos la detección de células para trabajar detectando células según su radio y también se arregló la detección parcial de las células a colores a completas

--- DetectColores.py
+++ DetectColores.py
@@ -1,9 +1,13 @@
 import cv2 
+import cv2 
 import numpy as np 
 import time
 
+
+flag=1212
+
 circless,circuloA,circuloR = 0,0,0
-image = cv2.imread('seq/Img000806.jpg')
+image = cv2.imread('seq/Img001212.jpg')
 start=time.perf_counter()
 # Pasamos las imágenes de BGR a: GRAY (esta a BGR nuevamente) y a HSV
 imagen_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -15,15 +19,17 @@ cv2.fillPoly(mask, pts =[contour], color=(255,255,255))
 masked_img = cv2.bitwise_and(mask,imagen_gray)
 blur = cv2.blur(masked_img, (7,5))
 circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1.05, 10,param1=20,param2=12,minRadius=7,maxRadius=12)
-circles = np.uint16(np.around(circles))
 
-for i in circles[0,:]:
-    cv2.circle(image, (i[0], i[1]), i[2], (255, 255, 255), 1)
-    circless+=1
-
+if circles is not None:
+    circles = np.uint16(np.around(circles))
+    for i in circles[0,:]:
+        cv2.circle(image, (i[0], i[1]), i[2], (255, 255, 255), 1)
+        circless+=1
+    
 circles2 = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 2, 10, param1=100, param2=50, minRadius=25, maxRadius=40)
-circles2 = np.uint16(np.around(circles2))
+
 if circles2 is not None: 
+    circles2 = np.uint16(np.around(circles2))
     for i in circles2[0,:]:
         b,g,r = image[i[1], i[0]]
         if (b > r) :
